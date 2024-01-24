e345a3d44d3e8ed7591a46a345b14cd4ce88b701
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2024-01-24 17:43:45 +0000

    Ready

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,8 +4,8 @@ import "../../styles/home.css";
 import { Link } from "react-router-dom";
 
 export const Home = () => (
-	<div className="text-center Titular TitularX">
-		<h1 className="display-1 fw-bold">Adrian Rios Ruiz</h1>
+	<div className="text-center Titular  ">
+		<h1 className="display-1 fw-bold efectoOptico">Adrian Rios Ruiz</h1>
 		<Link className="display-5" to="/contact">
 			<h4>Check your Contacts</h4>
 		</Link>
